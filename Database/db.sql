771838e49ee90b93d1a87aad223000d2e5ad7059
Charbel Karam <charbeljihadkaram@gmail.com>
2024-04-15 17:23:52 +0300

    Update Readme.md

--- Database/db.sql
+++ Database/db.sql
@@ -1,36 +1,3 @@
-# Project Name
-Camping Website
-
-# Project Team Members
-* ABOU HAIDAR Elie Salim
-* EL NGHAYWE Miled Miled
-* FREM Anna - Magdalena Michel
-* KARAM Charbel Jihad 
-
-# Project Descripton
-This project showcases a camping website built using HTML, CSS, JAVASCRIPT, PHP and SQL. 
-Where you can find, review, explore and book an exciting camping experience. 
-Remember, every adventure begins with a single click!  
-
-
-# Database Structure
-User (User-id, first_name, Last_name, email, password, phone_number, address, username, is_admin, creation_date)
-
-Reservation (reservation_id, total_price, from_date, to_date, #user-id, #Campsite-id)
-
-Campsite (campsite_id, name, capacity, #category_id, #Location_id)
-
-Category (Category_id, name)
-
-Location (Location_id, city, district, governorate, laltitude, longitude)
-
-Price category (price_category_id, weekday_price, weekend_price, highseason_price)
-
-Review (review_id, rating, review text, date_posted, #campsite_id)
-
-Campsite_Price (#campsite_id, #price_category_id, price)
-
-## Table: listing
 CREATE TABLE user (
     user_id INT not null auto_increment,
     first_name VARCHAR(15) not null,
@@ -128,6 +95,3 @@ CREATE TABLE campsite_price_category(
     ON UPDATE CASCADE,
     ON DELETE SET NULL
 );
-
-# Meet Link
-https://teams.live.com/meet/9468647618631?p=ZcQYDMTAyJeszmW3 
